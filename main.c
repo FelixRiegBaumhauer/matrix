38ff8de8405ecb4532cb574984bbce97d4a91528
riegbaumhauer <riegbaumhauer@gmail.com>
2017-02-28 15:33:30 -0500

    last fxn

--- main.c
+++ main.c
@@ -23,6 +23,18 @@ int main() {
 
   scalar_mult(2, edges);
   print_matrix(edges);
+
+  struct matrix *a;
+  a = new_matrix(4, 2);
+  printf("%d\n", a->lastcol);
+
+  add_point(a, 3, 4, 5);
+  add_point(a, 1, 2, 3);
+  add_point(a, 37, 47, 750);
+  print_matrix(a);
+  add_edge(a, 34, 34, 34, 56, 56, 56);
+  print_matrix(a);
+  printf("%d\n", a->lastcol);
   
   /*
   struct matrix *a;
